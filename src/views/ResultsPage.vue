002f0845344f7075755df785b3cf40ddd6b8ba8c
Davi Bressiani <davi.bressiani@fatec.sp.gov.br>
2021-07-27 11:11:51 -0300

    feat: Added API_TOKEN

--- src/views/ResultsPage.vue
+++ src/views/ResultsPage.vue
@@ -19,7 +19,13 @@ export default {
     },
 
     created() {
-        fetch("https://www.receitaws.com.br/v1/cnpj/04890965000158")
+        const API_TOKEN = "d2d52cd33eedaf3c62539e03a9b7ee599c94cce42fb2282a68f92908ea2a63e6";
+
+        fetch("https://www.receitaws.com.br/v1/cnpj/04890965000158/days/10", {
+            headers: {
+                Authorization: `Bearer ${API_TOKEN}`,
+            },
+        })
             .then((response) => response.json())
             .then((json) => {
                 this.cnpjResults = json;
