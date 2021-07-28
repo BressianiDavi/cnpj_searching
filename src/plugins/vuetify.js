65df4264abacac4d67bbe7b5c619d21710dd32f5
Davi Bressiani <davi.bressiani@fatec.sp.gov.br>
2021-07-28 01:28:21 -0300

    feat: added vue-mask to mask CNPJ

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,8 +6,4 @@ Vue.use(VMask);
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
-    icons: {
-        iconfont: "mdiSvg", // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
-    },
-});
+export default new Vuetify({});
