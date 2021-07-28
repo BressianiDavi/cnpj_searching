e31773406ea8ac9b878e7254d38a84bb47a32e10
Davi Bressiani <davi.bressiani@fatec.sp.gov.br>
2021-07-28 14:41:17 -0300

    feat: displayed all informations using loops

--- src/components/ListResults.vue
+++ src/components/ListResults.vue
@@ -1,14 +1,15 @@
 <template>
     <v-card class="mx-auto vcard" width="40rem" style="margin-top: 3rem;">
-        
         <v-divider></v-divider>
 
         <v-card-text>
             <div class="my-4 text-subtitle-1 " v-if="cnpjData.nome">Nome da Empresa: {{ cnpjData.nome }}</div>
             <div class="my-4 text-subtitle-1 " v-else>Nome da Empresa: --</div>
 
-            <div class="my-4 text-subtitle-1 " v-if="cnpjData.atividade_principal[0]">
-                Atividade Principal: {{ cnpjData.atividade_principal[0].text }}
+            <div class="my-4 text-subtitle-1 " v-if="cnpjData.atividade_principal">
+                <p v-for="(atividade, index) of cnpjData.atividade_principal" :key="atividade.text">
+                    Atividade Principal {{ index + 1 }}: {{ atividade.text }} (código: {{ atividade.code }})
+                </p>
             </div>
             <div class="my-4 text-subtitle-1 " v-else>
                 Atividade Principal: --
@@ -19,8 +20,10 @@
             <div class="my-4 text-subtitle-1 " v-if="cnpjData.tipo">Tipo: {{ cnpjData.tipo }}</div>
             <div class="my-4 text-subtitle-1 " v-else>Tipo: --</div>
 
-            <div class="my-4 text-subtitle-1 " v-if="cnpjData.atividades_secundarias[0]">
-                Atividades Secundárias: {{ cnpjData.atividades_secundarias[0].text }}
+            <div class="my-4 text-subtitle-1 " v-if="cnpjData.atividades_secundarias">
+                <p v-for="(atividade, index) of cnpjData.atividades_secundarias" :key="atividade.text">
+                    Atividades Secundárias {{ index + 1 }}: {{ atividade.text }} (código: {{ atividade.code }})
+                </p>
             </div>
             <div class="my-4 text-subtitle-1 " v-else>
                 Atividades Secundárias: --
@@ -33,7 +36,12 @@
             <div class="my-4 text-subtitle-1 " v-if="cnpjData.situacao">Situação: {{ cnpjData.situacao }}</div>
             <div class="my-4 text-subtitle-1 " v-else>Situação: --</div>
 
-            <div class="my-4 text-subtitle-1 ">Endereço:</div>
+            <div class="my-4 text-subtitle-1 " v-if="cnpjData.logradouro && cnpjData.numero && cnpjData.bairro">
+                Endereço: {{ cnpjData.logradouro }}, {{ cnpjData.numero }} - {{ cnpjData.bairro }}
+            </div>
+            <div class="my-4 text-subtitle-1 " v-else>
+                Endereço: Não informado
+            </div>
 
             <div class="my-4 text-subtitle-1 " v-if="cnpjData.municipio && cnpjData.uf">
                 {{ cnpjData.municipio }} - {{ cnpjData.uf }}
@@ -41,15 +49,6 @@
             <div class="my-4 text-subtitle-1 " v-else>
                 Cidade - UF: Não informado
             </div>
-
-            <div class="my-4 text-subtitle-1 " v-if="cnpjData.logradouro">
-                {{ cnpjData.logradouro }} - {{ cnpjData.numero }}
-            </div>
-            <div class="my-4 text-subtitle-1 " v-else>
-                Rua e N°: Não informado
-            </div>
-            <div class="my-4 text-subtitle-1 " v-if="cnpjData.bairro">{{ cnpjData.bairro }}</div>
-            <div class="my-4 text-subtitle-1 " v-else>Bairro: --</div>
 
             <div class="my-4 text-subtitle-1 " v-if="cnpjData.cep">CEP: {{ cnpjData.cep }}</div>
             <div class="my-4 text-subtitle-1 " v-else>CEP: --</div>
@@ -65,6 +64,8 @@ export default {
         cnpjData: {
             required: true,
         },
+
+        loading: {},
     },
     data() {
         return {
@@ -74,4 +75,9 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.endereco {
+    display: flex;
+    padding: 0 0 0 0;
+}
+</style>
