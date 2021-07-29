6f541b65987926ef8f8b0d255e8620e07307ad76
Davi Bressiani <davi.bressiani@fatec.sp.gov.br>
2021-07-28 23:47:17 -0300

    feat: WIP show message error if the requisition failed

--- src/views/ResultsPage.vue
+++ src/views/ResultsPage.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
         <div v-if="isLoading"><Loader /></div>
-        <div v-else><ListResults :cnpjData="cnpjResults" /></div>
+        <div v-else-if="hasError">Não foi possível, tente novamente mais tarde</div>
+        <div v-else>
+            <ListResults :cnpjData="cnpjResults" />
+        </div>
     </div>
 </template>
 
@@ -22,16 +25,21 @@ export default {
         return {
             cnpjResults: [],
             isLoading: false,
+            hasError: false,
         };
     },
+
+    // TODO:mostrar a mensagem de erro quando a requisição falhar
 
     created() {
         this.isLoading = true;
         jsonp("https://www.receitaws.com.br/v1/cnpj/" + this.$route.query.cnpj, null, (error, data) => {
             if (error) {
                 console.error("error on created results page", error);
+                this.hasError = true;
                 this.cnpjResults = [];
             } else {
+                this.hasError = false;
                 this.cnpjResults = data;
             }
             this.isLoading = false;
