d9f32fb54d178926779f705534efba5a697ad585
Davi Bressiani <davi.bressiani@fatec.sp.gov.br>
2021-07-29 10:50:31 -0300

    feat: displayed error if requisition failed

--- src/views/ResultsPage.vue
+++ src/views/ResultsPage.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
         <div v-if="isLoading"><Loader /></div>
-        <div v-else-if="hasError">Não foi possível, tente novamente mais tarde</div>
+        <div v-else-if="hasError">
+            <v-card class="mx-auto vcard" width="40rem" style="margin-top: 3rem;">
+                Falha ao as buscar informações no servidor, tente novamente em alguns minutos.</v-card
+            >
+        </div>
         <div v-else>
             <ListResults :cnpjData="cnpjResults" />
         </div>
@@ -29,17 +33,18 @@ export default {
         };
     },
 
-    // TODO:mostrar a mensagem de erro quando a requisição falhar
-
     created() {
         this.isLoading = true;
         jsonp("https://www.receitaws.com.br/v1/cnpj/" + this.$route.query.cnpj, null, (error, data) => {
             if (error) {
                 console.error("error on created results page", error);
+                window.alert(
+                    "Falha ao buscar informações no servidor, aguarde alguns minutos e tente novamente",
+                    error
+                );
                 this.hasError = true;
                 this.cnpjResults = [];
             } else {
-                this.hasError = false;
                 this.cnpjResults = data;
             }
             this.isLoading = false;
