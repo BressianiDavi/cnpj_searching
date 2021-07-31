7d09eaf2b88bc8d3688b61ee671970201c7fc5f8
Davi Bressiani <davi.bressiani@fatec.sp.gov.br>
2021-07-31 11:46:44 -0300

    feat: added icons in the tool bar

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,9 +1,28 @@
 <template>
     <v-card flat>
         <v-toolbar color="primary" dark extended flat>
-            <h1>
+            <h1 style="font-size: 2rem">
                 Consulta de CNPJ
             </h1>
+            <div class="icons">
+                <router-link to="/">
+                    <v-list-item>
+                        <v-list-item-icon>
+                            <v-icon>mdi-home</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Home</v-list-item-title>
+                    </v-list-item>
+                </router-link>
+
+                <router-link to="/history">
+                    <v-list-item>
+                        <v-list-item-icon>
+                            <v-icon>mdi-history</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>History</v-list-item-title>
+                    </v-list-item>
+                </router-link>
+            </div>
         </v-toolbar>
     </v-card>
 </template>
@@ -17,5 +36,11 @@ export default {
 <style scoped>
 h1 {
     font-size: 2rem;
+}
+
+.icons {
+    display: flex;
+    width: 80%;
+    justify-content: flex-end;
 }
 </style>
