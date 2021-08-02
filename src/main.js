ea107d76e7658d210797a3f2bad484df25d0ea29
Davi Bressiani <davi.bressiani@fatec.sp.gov.br>
2021-08-02 00:22:25 -0300

    feat: added global filter pt-BR date fns in the main.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
+import { createDateFilter } from "vue-date-fns";
+import { pt } from "date-fns/locale";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.filter("date", createDateFilter("dd 'de' MMMM 'de' yyyy 'as' HH:mm", { locale: pt }));
 
 new Vue({
-  vuetify,
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    vuetify,
+    router,
+    render: (h) => h(App),
+}).$mount("#app");
