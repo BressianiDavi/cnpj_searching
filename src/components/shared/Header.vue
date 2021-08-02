371d24a3b89dca4e67971b2a60f00cb406cc26d9
Davi Bressiani <davi.bressiani@fatec.sp.gov.br>
2021-08-02 19:24:27 -0300

    styles: change the color of Header and adjust css of icons

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,24 +1,24 @@
 <template>
-    <v-card flat>
-        <v-toolbar color="primary" dark extended flat>
+    <v-card style="border-radius: 0" flat>
+        <v-toolbar color="indigo" dark extended flat>
             <v-row xs>
-                <h1 style="font-size: 2rem">
+                <h1 style="margin-left: 1rem">
                     Consulta de CNPJ
                 </h1>
             </v-row>
             <v-row class="icons">
-                <router-link to="/">
+                <router-link to="/" style="text-decoration: none">
                     <v-list-item>
-                        <v-list-item-icon>
+                        <v-list-item-icon style="margin-right: 0.5rem">
                             <v-icon>mdi-home</v-icon>
                         </v-list-item-icon>
                         <v-list-item-title>Home</v-list-item-title>
                     </v-list-item>
                 </router-link>
 
-                <router-link to="/history">
+                <router-link to="/history" style="text-decoration: none">
                     <v-list-item>
-                        <v-list-item-icon>
+                        <v-list-item-icon style="margin-right: 0.5rem">
                             <v-icon>mdi-history</v-icon>
                         </v-list-item-icon>
                         <v-list-item-title>History</v-list-item-title>
